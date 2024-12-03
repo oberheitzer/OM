3c4d8a54be3ed1e3a8c1ab2fc88d3684bbe2d07f
oberheitzer <bertalan.david.krisztian@gmail.com>
2024-12-04 00:06:25 +0100

    add main project

---
+++ OM.Main/Helpers/Starter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OM.Main;
+
+public static class Starter
+{
+    public static ServiceProvider Run()
+    {
+        var services = new ServiceCollection();
+        services.AddServices();
+        return services.BuildServiceProvider();
+    }
+}
---
+++ OM.Main/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OM.Domain;
+using OM.Library;
+
+namespace OM.Main;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var provider = Starter.Run();
+
+        if (args.Length > 0)
+        {
+            var weatherService = provider.GetRequiredService<IWeatherService>();
+            var storeService = provider.GetRequiredService<IStoreService>();
+            if (args[0] == "-f")
+            {
+                WeatherData data = await weatherService.GetForecastAsync();
+                storeService.Save(data: data, folder: "forecast");
+            }
+            else if (args[0] == "-s" && args.Length == 2 && DateOnly.TryParse(args[1], out DateOnly day))
+            {
+                WeatherData data = await weatherService.GetHistoryWeatherAsync(day: day);
+                storeService.Save(data: data, folder: "actual");
+            }
+            else
+            {
+                Console.WriteLine("Invalid arguments.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Missing arguments.");
+        }
+    }
+}
